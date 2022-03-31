345c00a5c48addf2fc51dcf1617006a64ca02130
michelle huang <michelle80098@gmail.com>
2022-03-31 09:23:28 -0600

    brick update

---
+++ game/Casting/map.py
@@ -0,0 +1,35 @@
+from game.Casting.actor import Actor
+
+# The layout of the game, creating objects and walls within the game to build the course of the game
+
+class Map():
+    """A solid, rectangular object that can be broken."""
+
+    def __init__(self, body, animation, points, debug = False):
+        """Constructs a new Brick.
+        
+        Args:
+            body: A new instance of Body.
+            image: A new instance of Image.
+            debug: If it is being debugged. 
+        """
+        super().__init__(debug)
+        self._body = body
+        self._animation = animation
+        self._points = points
+        
+    def get_animation(self):
+        """Gets the brick's image.
+        
+        Returns:
+            An instance of Image.
+        """
+        return self._animation
+
+    def get_body(self):
+        """Gets the brick's body.
+        
+        Returns:
+            An instance of Body.
+        """
+        return self._body
